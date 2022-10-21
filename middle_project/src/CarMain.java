b3d132c3b084a12f436c724b4a8bfa292367c150
kimbyeonggeun <qudrms2157@gmail.com>
2022-10-21 19:46:35 +0900

    java파일 틀 작성

--- middle_project/src/CarMain.java
+++ middle_project/src/CarMain.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class CarMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("-------------------------------------------------------");
             System.out.println("P.설문시작 | S.통계 | D.삭제 | Q.종료 ");
             System.out.println("-------------------------------------------------------");
 
-            Scanner sc = new Scanner(System.in);
             System.out.print("선택 > ");
             String choose = sc.next();
 
@@ -25,5 +25,6 @@ public class CarMain {
                 break;
             }
         }
+        sc.close();
     }
 }
