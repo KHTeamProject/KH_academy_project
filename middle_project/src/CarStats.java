c2db4bc44d66a0352b4390f995091a81c918dedd
yongbeom76 <yongbeom76@gmail.com>
2022-10-24 13:56:09 +0900

    CarStats

--- middle_project/src/CarStats.java
+++ middle_project/src/CarStats.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class CarStats { //통계 김용범 
     public void stats(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. 설문자별 답변 결과 | 2. 질문별 총 답변수");
+        System.out.println("선택) ");
+        String answer = sc.next();
+        
     }
 }
