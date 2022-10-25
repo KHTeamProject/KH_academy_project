782e344bb711d7d7fd3cea13094ae0a9193a2eff
Giljinsu <therin234@gmail.com>
2022-10-25 13:34:05 +0900

    11

--- middle_project/src/CarDelete.java
+++ middle_project/src/CarDelete.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class CarDelete { // 삭제
     Scanner sc = new Scanner(System.in);
-    Statement statement;
+    Statement statement,statement2;
     ResultSet resultSet;
 
 
@@ -13,12 +13,14 @@ public class CarDelete { // 삭제
         String uId = sc.nextLine();
         System.out.print("비밀번호를 입력해주세요. > ");
         String uPw = sc.nextLine();
+        int fin = 0;
 
         String query = "SELECT * FROM USERS";
         
 
         try {
             statement = conn.createStatement();
+            statement2 = conn.createStatement();
             resultSet = statement.executeQuery(query);
 
             //불러오기
@@ -29,24 +31,24 @@ public class CarDelete { // 삭제
                 deleteQueryUser = "DELETE FROM users WHERE USER_ID = "+"'"+userId+"'";
                 String deleteQueryUserAnswer = "DELETE FROM users_answer WHERE USER_ID = "+"'"+userId+"'";
 
-                if(uId.equals(userId) && uPw.equals(userPw) ) { // 일치 할시
+
+                if(uId.equals(userId) && uPw.equals(userPw)) { // 일치 할시
                 // 설문삭제 코드
 
-                statement.execute(deleteQueryUserAnswer);
-                statement.execute(deleteQueryUser);
+                statement2.execute(deleteQueryUserAnswer);
+                statement2.execute(deleteQueryUser);
                 
              
                 System.out.println("설문이 삭제되었습니다.");
-                resultSet.close();
-                break;
-                } else {
-                System.out.println("일치하지 않습니다.");
-                resultSet.close();
-                break;
-                
+                statement2.close();
+                fin=1;
+
                 } 
-                
             }
+                if(fin == 0)
+                System.out.println("일치하지 않습니다.");
+
+            
             
             //삭제
             
